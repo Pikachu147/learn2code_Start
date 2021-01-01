4c126d9bfa83d34c06930079ebcd708a674a132e
Rastislav <rastislav.tatarko@gmail.com>
2021-01-01 14:14:12 +0100

    Vstupne subory projektu

---
+++ src/main/java/com/luigicoctails/LuigiCoctails/LuigiCoctailsApplication.java
@@ -0,0 +1,13 @@
+package com.luigicoctails.LuigiCoctails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuigiCoctailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuigiCoctailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luigicoctails/LuigiCoctails/LuigiCoctailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luigicoctails.LuigiCoctails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuigiCoctailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
